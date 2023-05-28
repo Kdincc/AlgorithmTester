a9400ae7ad554fdecec2c71f4bc900647c859c6e
Kostya Dermenzhi <dermenzhi.kostya@gmail.com>
2023-05-29 01:01:20 +0300

    dev

--- AlgorithmTester/MainWindow.xaml.cs
+++ AlgorithmTester/MainWindow.xaml.cs
@@ -8,9 +8,6 @@ using System.Windows.Input;
 
 namespace AlgorithmTester
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         private const int SLIDER_VALUE_MULTIPLIER = 100;
