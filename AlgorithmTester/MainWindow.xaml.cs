a96044b6a85b23e8c5a9cd6e88f5f8a97e63dbe7
Kostya Dermenzhi <dermenzhi.kostya@gmail.com>
2023-06-03 15:03:36 +0300

    DEV

--- AlgorithmTester/MainWindow.xaml.cs
+++ AlgorithmTester/MainWindow.xaml.cs
@@ -3,7 +3,6 @@ using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Linq;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Input;
 
 namespace AlgorithmTester
